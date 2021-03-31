0e7e6d5342381f0884a29fabfe85032d991a7bb6
amrita tiwari <66867993+Amrita-Tiwari@users.noreply.github.com>
2021-03-31 19:19:35 +0530

    Add files via upload

---
+++ CandidateDbQuery.sql
@@ -0,0 +1,4 @@
+create database CandidateCrudTestScore;
+use CandidateCrudTestScore;
+create table Candidate(id varchar(10), name varchar(30), email varchar(50));
+create table test_score(first_round varchar(5), second_round varchar(5), third_round varchar(5));
